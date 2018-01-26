70bdd8403966b9cea52cbc1e5497f2e4e1533721
Ashley Davies <ashley@daviesl.uk>
2018-01-26 01:47:45 +0000

    Update CapturePlayerHeuristic to want cities

--- GeneralsBot/TargetHeuristics/CapturePlayerHeuristic.cs
+++ GeneralsBot/TargetHeuristics/CapturePlayerHeuristic.cs
@@ -11,6 +11,11 @@ namespace GeneralsBot.TargetHeuristics {
                         || map[x, y] is FogTile f && f.Seen && f.KnownType == typeof(GeneralTile)) {
                         desireds.Add((100000000, new Position(x, y)));
                     }
+                    
+                    if (map[x, y] is CityTile cTile && cTile.Faction >= 0 && cTile.Faction != playerIndex
+                     || map[x, y] is FogTile cF && cF.Seen && cF.KnownType == typeof(CityTile)) {
+                        desireds.Add((100, new Position(x, y)));
+                    }
                 }
             }
             
